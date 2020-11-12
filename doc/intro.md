8a326f6306d2d020ef733fc9edddc07e1f667bdb
Pablo Sanchez Alonso <pablo.sanchez@codurance.com>
2020-11-12 10:49:21 +0000

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to workable-leankit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/workable_leankit/core_test.clj
@@ -0,0 +1,7 @@
+(ns workable-leankit.core-test
+  (:require [clojure.test :refer :all]
+            [workable-leankit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
