82605e1f84ab4701b95cb6dde153e39d151824c0
Pablo Sanchez Alonso <pablo.sanchez@codurance.com>
2020-11-12 15:38:22 +0000

    Implemented creation of the board
    The creation of the board has been implemented as well as the failure
    case of gathering the  stages from the Workable API. The work is still
    in progress and, as a result, the call to the workable API hasn't been
    implemented yet.

--- src/workable_leankit/core.clj
+++ src/workable_leankit/core.clj
@@ -1,6 +1,17 @@
-(ns workable-leankit.core)
+(ns workable-leankit.core
+  (:require [clj-http.client :as http]
+            [clojure.stacktrace :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def leankit-url (System/getenv "LEANKIT_URL"))
+(def leankit-token (System/getenv "LEANKIT_TOKEN"))
+
+(defn main 
+  "Synchronises Workable with a Leankit board"
+  [& args]
+  (try 
+    (http/post (str leankit-url "/io/board")
+               {:bearer-auth leankit-token})
+    {:status :error
+     :message "Failed to get stages"}
+    (catch clojure.lang.ExceptionInfo e
+      {:status :error :message "Failed to create board"})))
--- test/workable_leankit/core_test.clj
+++ test/workable_leankit/core_test.clj
@@ -1,7 +1,33 @@
 (ns workable-leankit.core-test
   (:require [clojure.test :refer :all]
-            [workable-leankit.core :refer :all]))
+            [workable-leankit.core :refer :all]
+            [clj-http.client :as http]
+            [clj-http-mock.core :as http-mock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest workable-leankit-test
+  (testing "Errors creating the board"
+    (http-mock/with-mock-routes
+      [(http-mock/route 
+         :post 
+         (str (System/getenv "LEANKIT_URL") "/io/board"))
+       (constantly {:status 403 
+                    :body "{\"statusCode\": 403, \"message\": \"Forbidden\"}"})]
+      (is (= {:status :error
+              :message "Failed to create board"} 
+             (main)))))
+  (testing "Errors gettting the stages"
+    (http-mock/with-mock-routes
+      [(http-mock/route 
+         :post
+         (str (System/getenv "LEANKIT_URL") "/io/board"))
+       (constantly {:status 200 
+                    :body "{\"id\": 123}"})]
+      [(http-mock/route 
+         :get
+         (str (System/getenv "WORKABLE_URL") "/stages"))
+       (constantly {:status 401
+                    :body {:error "Not authorized"}})]
+      (is (= {:status :error
+              :message "Failed to get stages"}
+             (main))))))
+
