35a0debea5ba77bc399888625821794183aed9d7
Pablo Sanchez Alonso <pablo.sanchez@codurance.com>
2020-11-16 12:07:29 +0000

    Added API call to get stages (not finished)

--- src/workable_leankit/core.clj
+++ src/workable_leankit/core.clj
@@ -1,16 +1,35 @@
 (ns workable-leankit.core
   (:require [clj-http.client :as http]
-            [clojure.stacktrace :refer :all]))
+            [clojure.stacktrace :refer :all]
+            [clojure.data.json :as json]))
 
 (def leankit-url (System/getenv "LEANKIT_URL"))
 (def leankit-token (System/getenv "LEANKIT_TOKEN"))
+(def workable-url (System/getenv "WORKABLE_URL"))
+(def workable-token (System/getenv "WORKABLE_TOKEN"))
+
+(defn status-try [f message]
+  (try (f) 
+       (catch clojure.lang.ExceptionInfo e
+         {:status :error :message message})))
 
 (defn main 
   "Synchronises Workable with a Leankit board"
   [& args]
   (try 
-    (http/post (str leankit-url "/io/board")
-               {:bearer-auth leankit-token})
-    {:status :error :message "Failed to get stages"}
+    (let [board-id-response 
+          (http/post (str leankit-url "/io/board")
+                     {:bearer-auth leankit-token})
+          board-id-map (json/read-str (:body board-id-response)
+                                      :key-fn keyword)
+          board-id (:id board-id-map)]
+
+      (status-try (fn []
+                    (http/get (str workable-url "/stages")
+                              {:bearer-auth workable-token})
+                    {:status :error
+                     :message "Failed to add stages to board"})
+                  "Failed to get stages"))
     (catch clojure.lang.ExceptionInfo e
       {:status :error :message "Failed to create board"})))
+
--- test/workable_leankit/core_test.clj
+++ test/workable_leankit/core_test.clj
@@ -21,13 +21,35 @@
          :post
          (str (System/getenv "LEANKIT_URL") "/io/board"))
        (constantly {:status 200 
-                    :body "{\"id\": 123}"})]
-      [(http-mock/route 
+                    :body "{\"id\": 123}"})
+       (http-mock/route 
          :get
          (str (System/getenv "WORKABLE_URL") "/stages"))
        (constantly {:status 401
                     :body "{\"error\": \"Not authorized\"}"})]
       (is (= {:status :error
               :message "Failed to get stages"}
+             (main)))))
+
+  (testing "Errors add a stage on the board"
+    (http-mock/with-mock-routes
+      [(http-mock/route 
+         :post
+         (str (System/getenv "LEANKIT_URL") "/io/board"))
+       (constantly {:status 200 
+                    :body "{\"id\": 123}"})
+       (http-mock/route 
+         :get
+         (str (System/getenv "WORKABLE_URL") "/stages"))
+       (constantly {:status 200
+                    :body "{\"stages\": [{\"name\": \"phone interview\"}]}"})
+       (http-mock/route 
+         :get
+         (str (System/getenv "LEANKIT_URL") "/board/123/layout"))
+       (constantly {:status 401
+                    :body "{\"statusCode\": 403, \"message\": \"Forbidden\"}"})]
+      (is (= {:status :error
+              :message "Failed to add stages to board"}
              (main))))))
+
 
