07d9f81502668ae51d22f81e4641daebeaf4948e
Pablo Sanchez Alonso <pablo.sanchez@codurance.com>
2020-11-17 13:45:33 +0000

    Implemented request to save the stages as lanes
    Now, the function will create lanes in the board based on the stages
    that were taken from Workable.

--- src/workable_leankit/core.clj
+++ src/workable_leankit/core.clj
@@ -13,23 +13,41 @@
        (catch clojure.lang.ExceptionInfo e
          {:status :error :message message})))
 
+(defn migrate-lanes-to-leankit [board-id lanes-body]
+  (println lanes-body)
+  (http/put (str leankit-url "/io/board/" board-id "/layout")
+            {:bearer-auth leankit-token
+             :body lanes-body})
+  {:status :error :message "Failed to get candidates"})
+
+(defn get-stages [board-id]
+  (let [stages-response
+        (http/get (str workable-url "/stages")
+                  {:bearer-auth workable-token })
+
+        stages-map (json/read-str (:body stages-response)
+                                  :key-fn keyword)
+        stages (:stages stages-map)
+        lanes (map (fn [item] {:title (:name item)}) stages)
+        lanes-body (json/write-str {:lanes lanes})]
+
+    (println lanes-body)
+    (status-try #(migrate-lanes-to-leankit board-id lanes-body)
+                 "Failed to add stages to board")))
+
+(defn migrate-workable-to-leankit []
+  (let [board-id-response 
+        (http/post (str leankit-url "/io/board")
+                   {:bearer-auth leankit-token})
+        board-id-map (json/read-str (:body board-id-response)
+                                    :key-fn keyword)
+        board-id (:id board-id-map)]
+
+    (status-try #(get-stages board-id)
+                "Failed to get stages")))
+
 (defn main 
   "Synchronises Workable with a Leankit board"
   [& args]
-  (try 
-    (let [board-id-response 
-          (http/post (str leankit-url "/io/board")
-                     {:bearer-auth leankit-token})
-          board-id-map (json/read-str (:body board-id-response)
-                                      :key-fn keyword)
-          board-id (:id board-id-map)]
-
-      (status-try (fn []
-                    (http/get (str workable-url "/stages")
-                              {:bearer-auth workable-token})
-                    {:status :error
-                     :message "Failed to add stages to board"})
-                  "Failed to get stages"))
-    (catch clojure.lang.ExceptionInfo e
-      {:status :error :message "Failed to create board"})))
+  (status-try migrate-workable-to-leankit "Failed to create board"))
 
--- test/workable_leankit/core_test.clj
+++ test/workable_leankit/core_test.clj
@@ -44,12 +44,39 @@
        (constantly {:status 200
                     :body "{\"stages\": [{\"name\": \"phone interview\"}]}"})
        (http-mock/route 
-         :get
-         (str (System/getenv "LEANKIT_URL") "/board/123/layout"))
+         :put
+         (str (System/getenv "LEANKIT_URL") "/io/board/123/layout"))
        (constantly {:status 401
                     :body "{\"statusCode\": 403, \"message\": \"Forbidden\"}"})]
       (is (= {:status :error
               :message "Failed to add stages to board"}
-             (main))))))
+             (main)))))
+  (testing "Error fetching candidates"
+    (http-mock/with-mock-routes
+      [(http-mock/route 
+         :post
+         (str (System/getenv "LEANKIT_URL") "/io/board"))
+       (constantly {:status 200 
+                    :body "{\"id\": 123}"})
+       (http-mock/route 
+         :get
+         (str (System/getenv "WORKABLE_URL") "/stages"))
+       (constantly {:status 200
+                    :body "{\"stages\": [{\"name\": \"phone interview\"}]}"})
+       (http-mock/route 
+         :put
+         (str (System/getenv "LEANKIT_URL") "/io/board/123/layout"))
+       (constantly {:status 200
+                    :body "{ \"lanes\": [ { \"title\": \"phone interview\", \"wipLimit\": 0, \"columns\": 1, \"orientation\": \"vertical\", \"index\": 0, \"type\": \"ready\", \"classType\": \"backlog\", \"cardStatus\": \"notStarted\", \"description\": null, \"isConnectionDoneLane\": false, \"isDefaultDropLane\": false, \"children\": [] } ], \"layoutChecksum\": \"b41c4d1deb7e46b2180a636020b2e5cf\" }"})
+       (http-mock/route
+         :get
+         (str (System/getenv "WORKABLE_URL") "/candidates"))
+       (constantly {:status 401
+                    :body "{\"error\": \"not authorized\"}"})]
+      (is (= (main)
+             {:status :error :message "Failed to get candidates"}))))
+  )
+
+
 
 
